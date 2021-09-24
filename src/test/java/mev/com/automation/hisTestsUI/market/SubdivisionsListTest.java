d281555c3e8a0ee1ba613510e36b38df94e750e7
AlexKalyna <56594087+AlexKalyna@users.noreply.github.com>
2021-09-24 15:24:12 +0300

    Uncommented tests

--- src/test/java/mev/com/automation/hisTestsUI/market/SubdivisionsListTest.java
+++ src/test/java/mev/com/automation/hisTestsUI/market/SubdivisionsListTest.java
@@ -3,7 +3,7 @@ package mev.com.automation.hisTestsUI.market;
 import mev.com.automation.hisTestsUI.TestBase;
 import org.junit.jupiter.api.Test;
 
-public class SubdivisionsList extends TestBase {
+public class SubdivisionsListTest extends TestBase {
 
  @Test
   public void openSubdivisionsListPage(){
